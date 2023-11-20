84fd46f21567d4903d81d586911d66826ce9f02e
jack <jspiratos26@gmail.com>
2023-11-20 10:16:23 -0500

    quick save

---
+++ SpaceSurfers/Assets/Scripts/GroundTile.cs
@@ -0,0 +1,25 @@
+
+using System;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2); // will destroy the object 2 second after passing the trigger 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
